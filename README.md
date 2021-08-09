eb6a88b073daf4ffa5d411aa2d8cc8857f461f2d
huytv <huy8895@gmail.com>
2021-08-09 23:45:46 +0700

    v3

--- README.md
+++ README.md
@@ -4,6 +4,4 @@ json log to spring log
 - run: npm install
 
 cd to project: 
-`node app.js <skip-line> <file-path>`
-
-exampale `node app.js 5 "C:\Users\trinh\Downloads\loan-origination-67c8485998-qksxf_loan-origination (3).log"`
+`node app.js`
--- app.js
+++ app.js
@@ -1,52 +1,77 @@
 const fs = require('fs');
 const readline = require('readline');
 const moment = require('moment');  
-
+const path = require('path');
 
 const defaultInputFile = 'AFS-log.json';
 const defaultSkipLine = 5;
 const defaultOutputFile = 'out/AFS-new-logs.log';
 const outputFolder = 'out';
+const inputFolder = 'input';
 const breakLine = '\r\n';
 const outFileExtension = '.log';
 
 const skipLine = isNullOrUndefine(process.argv[2]) ? defaultSkipLine : process.argv[2];
-const outputFile = isNullOrUndefine(process.argv[3]) ? defaultOutputFile : (outputFolder + '/' + process.argv[3] + outFileExtension);
+const outputFile = isNullOrUndefine(process.argv[3]) ? defaultOutputFile : (folder + '/' + process.argv[3] + outFileExtension);
 const inputFile = isNullOrUndefine(process.argv[4]) ? defaultInputFile : process.argv[4];
 
-const writeStream = fs.createWriteStream(outputFile);
+createFolderIfNotExist(inputFolder);
+createFolderIfNotExist(outputFolder);
 
-const rl = readline.createInterface({
-    input: fs.createReadStream(inputFile),
-    crlfDelay: Infinity
-});
-
-if (!fs.existsSync(outputFolder)) {
-    fs.mkdirSync(outputFolder);
-}
-
-let count = 0;
-
-console.log(`skipLine = ${skipLine}`)
-console.log(`inputFile = ${inputFile}`)
-
-rl.on('line', (line) => {
-    count++;
-    if (isNullOrUndefine(skipLine)) {
-        convertLog(line);
-    } else {
-        if(count > skipLine){
-            convertLog(line);
-        }
+fs.readdirSync(outputFolder, (err, files) => {
+    if (err) throw err;
+    for (const file of files) {
+        fs.unlink(path.join(outputFolder, file), err => {
+            if (err) throw err;
+        });
     }
 });
-console.log(`done ! check new log in ${outputFile}`)
+const listFile = getListFile(inputFolder);
+
+listFile.forEach(file => {
+    const rl = getReadLine(inputFolder + '/' +file);
+    const outputFile = (outputFolder + '/' + file).replace(/\s+/g, '');
+    const writeStream = fs.createWriteStream(outputFile);
+    rl.on('line', (line) => {
+        if (isJson(line)) {
+            convertLog(line,writeStream);
+        }
+    });
+    console.log(`done ! check new log in ${outputFile}`)
+})
+
+/**
+ * @param file
+ */
+function getReadLine(file) {
+    return readline.createInterface({
+        input: fs.createReadStream(file),
+        crlfDelay: Infinity
+    });
+}
+function getListFile(inputFolder) {
+    const listFile = [];
+    fs.readdirSync(inputFolder).forEach(file => {
+        listFile.push(file)
+    });
+    return listFile;
+}
+
+function createFolderIfNotExist(folder) {
+    if (!fs.existsSync(folder)) {
+        fs.mkdirSync(folder);
+    }
+}
+
+function deleteAllFileInFolder(folder) {
+    
+}
 
 function isNullOrUndefine(toCheck) {
     return toCheck == null || toCheck == undefined;
 }
 
-function convertLog(rawLine) {
+function convertLog(rawLine,writeStream) {
     let parseToObject = JSON.parse(rawLine);
     let newFormatLine = toNewFormat(parseToObject);
     writeStream.write(newFormatLine + breakLine);
@@ -62,7 +87,23 @@ function converDate(toConvert){
     return moment(dt).format('DD-MM-yyyy HH:mm:ss.SSS');
 }
 
+function isJson(item) {
+    item = typeof item !== "string"
+        ? JSON.stringify(item)
+        : item;
 
+    try {
+        item = JSON.parse(item);
+    } catch (e) {
+        return false;
+    }
+
+    if (typeof item === "object" && item !== null) {
+        return true;
+    }
+
+    return false;
+}
 
 
 
