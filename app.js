18bb80a06be0ddf747d1bb13c035c4757b456e69
huytv <huy8895@gmail.com>
2021-08-10 09:24:34 +0700

    v4

--- app.js
+++ app.js
@@ -1,36 +1,22 @@
 const fs = require('fs');
 const readline = require('readline');
 const moment = require('moment');  
-const path = require('path');
+const fsExtra = require('fs-extra')
 
-const defaultInputFile = 'AFS-log.json';
-const defaultSkipLine = 5;
-const defaultOutputFile = 'out/AFS-new-logs.log';
 const outputFolder = 'out';
 const inputFolder = 'input';
 const breakLine = '\r\n';
 const outFileExtension = '.log';
 
-const skipLine = isNullOrUndefine(process.argv[2]) ? defaultSkipLine : process.argv[2];
-const outputFile = isNullOrUndefine(process.argv[3]) ? defaultOutputFile : (folder + '/' + process.argv[3] + outFileExtension);
-const inputFile = isNullOrUndefine(process.argv[4]) ? defaultInputFile : process.argv[4];
-
 createFolderIfNotExist(inputFolder);
 createFolderIfNotExist(outputFolder);
+fsExtra.emptyDirSync(outputFolder)
 
-fs.readdirSync(outputFolder, (err, files) => {
-    if (err) throw err;
-    for (const file of files) {
-        fs.unlink(path.join(outputFolder, file), err => {
-            if (err) throw err;
-        });
-    }
-});
 const listFile = getListFile(inputFolder);
 
 listFile.forEach(file => {
     const rl = getReadLine(inputFolder + '/' +file);
-    const outputFile = (outputFolder + '/' + file).replace(/\s+/g, '');
+    const outputFile = (outputFolder + '/' + file).replace(/\s+/g, '') + outFileExtension;
     const writeStream = fs.createWriteStream(outputFile);
     rl.on('line', (line) => {
         if (isJson(line)) {
